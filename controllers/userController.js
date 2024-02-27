b850c2c5a9ae71673ecfbe121b704db6d1f3fd1e
Triss Marsh <marshtriss@gmail.com>
2024-02-27 22:01:33 +0000

    feat: follow user

--- controllers/userController.js
+++ controllers/userController.js
@@ -93,9 +93,99 @@ const getProfile = asyncHandler(async(req, res) => {
             },
         });
     } catch (error) {
-
+        console.error("Error getting User Profile:", error);
+        return res.status(500).send({
+            message: "An error occurd getting User Profile",
+            error: error.message,
+        });
     }
     
 })
 
-module.exports = { registerUser, authUser, getProfile }
+// Follow & Unfollow
+const followUser = asyncHandler(async(req, res) => {
+    try {
+        const { username } = req.params;
+        const { authUserId, userId } = req.body
+
+        const authUser = await User.findById(authUserId)
+        if (!authUser) {
+            return res.status(404).send({ message: "Authenticated user not found" });
+        }       
+
+        const targetUser = await User.findById(userId);
+        if (!targetUser) {
+            return res.status(404).send({ message: "Target user not found" });        
+        }
+
+        if (targetUser._id.toString() === authUser._id.toString()) {
+            return res.status(400).send({ message: "You can't follow yourself"});
+        }
+
+        if (authUser.following.includes(targetUser._id)) {
+            return res.status(400).send({ message: "You are already following this user" });
+        }        
+
+        authUser.following.push(targetUser);
+        await authUser.save();
+        targetUser.followers.push(authUser);
+        await targetUser.save();
+
+        const updatedAuthUser = await User.findById(authUserId).populate('following followers');
+        const updatedTargetUser = await User.findById(userId).populate('following followers');
+
+        res.status(200).json({
+            message: `Following ${username}`,
+            authUser: updatedAuthUser,
+            targetUser: updatedTargetUser,
+        });
+
+
+    } catch (error) {
+        console.error("Error following user:", error);
+        return res.status(500).send({
+            message: "An error occurd following user",
+            error: error.message,
+        });
+    }
+    
+})
+
+const unfollowUser = asyncHandler(async(req, res) => {
+    try {
+        const { username } = req.params;
+        const { authUserId, userId } = req.body
+
+        const authUser = await User.findById(authUserId)
+        if (!authUser) {
+            return res.status(404).send({ message: "Authenticated user not found" });
+        }       
+
+        const targetUser = await User.findOne({ username }).select('_id');
+        if (!targetUser || targetUser._id !== userId) {
+            return res.status(404).send({ message: "Target user not found" });
+        
+        }
+
+        if (!authUser.following.includes(targetUser._id)) {
+            return res.status(400).send({ message: "You are not following this user" });
+        }
+
+        authUser.following.push(targetUser);
+        await authUser.save();
+        targetUser.followers.push(authUser._id);
+        await targetUser.save();
+
+        res.status(200).json({
+            message: `Unfollowed ${username}`
+        });
+    } catch (error) {
+        console.error("Error unfollowing user:", error);
+        return res.status(500).send({
+            message: "An error occurd unfollowing user",
+            error: error.message,
+        });
+    }    
+})
+
+module.exports = { registerUser, authUser, getProfile, followUser, unfollowUser }
--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization',]
 }));
 
 app.get("/", (req, res, next) => {
